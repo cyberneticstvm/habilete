b2c15af1496fb03c42fb60ee65175325932e3995
CYBER NETICS <cybernetics.me@outlook.com>
2022-11-14 16:53:00 +0530

    small updates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,11 +6,11 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title>Demo Law Firm | Porto - Multipurpose Website Template</title>	
+		<title>Habilete Learning Solutions - CADPOINT Partner | AutoCAD | Civil CAD | Mechanical CAD | Electrical CAD</title>	
 
-		<meta name="keywords" content="WebSite Template" />
-		<meta name="description" content="Porto - Multipurpose Website Template">
-		<meta name="author" content="okler.net">
+		<meta name="keywords" content="AutoCAD, Civil CAD, Mechanical CAD, Electrical CAD, Diploma Courses, Skill Development" />
+		<meta name="description" content="Habilete Learning Solutions - CADPOINT Partner | AutoCAD | Civil CAD | Mechanical CAD | Electrical CAD | Diploma Courses | Skill Development | Jain University">
+		<meta name="author" content="cybernetics.me">
 
 		<!-- Favicon -->
 		<link rel="shortcut icon" href="{{ public_path().'/img/favicon.ico' }}" type="image/x-icon" />
@@ -55,28 +55,170 @@
 		<script src="{{ public_path().'/vendor/modernizr/modernizr.min.js' }}"></script>
 
 	</head>
-	<body>
-        @yield("content")
-        <!-- Vendor -->
-		<script src="{{ public_path().'/vendor/plugins/js/plugins.min.js' }}"></script>
+	<body class="loading-overlay-showing" data-loading-overlay data-plugin-options="{'hideDelay': 500, 'effect': 'cubes'}">
+        <div class="loading-overlay">
+            <div class="bounce-loader">
+                <div class="cssload-thecube">
+                    <div class="cssload-cube cssload-c1"></div>
+                    <div class="cssload-cube cssload-c2"></div>
+                    <div class="cssload-cube cssload-c4"></div>
+                    <div class="cssload-cube cssload-c3"></div>
+                </div>
+            </div>
+        </div>
+        <div class="body">
+            <header id="header" data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyStartAt': 107, 'stickySetTop': '-107px', 'stickyChangeLogo': true}">
+            <div class="header-body border-color-primary border-top-0 box-shadow-none">
+                <div class="header-container container z-index-2">
+                    <div class="header-row">
+                        <div class="header-column">
+                            <div class="header-row">
+                                <div class="header-logo">
+                                    <a href="demo-law-firm.html">
+                                        <img alt="Porto" width="125" height="47" src="img/demos/law-firm/logo.png">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-column justify-content-end">
+                            <div class="header-row h-100">
+                                <ul class="header-extra-info d-flex h-100 align-items-center">
+                                    <li class="align-items-center d-none d-lg-block h-100 py-4">
+                                        <div class="header-extra-info-text h-100 py-2">
+                                            <div class="feature-box feature-box-style-2 align-items-center">
+                                                <div class="feature-box-icon">
+                                                    <img width="34" height="28" src="img/demos/law-firm/icons/envelope.svg" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-primary'}" style="width: 34px; height: 28px;" />
+                                                </div>
+                                                <div class="feature-box-info ps-1">
+                                                    <label>SEND US AN EMAIL</label>
+                                                    <strong><a href="mailto:info@habilete.co.in">info@habilete.co.in</a></strong>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="align-items-center d-none d-lg-block h-100 py-4">
+                                        <div class="header-extra-info-text h-100 py-2">
+                                            <div class="feature-box feature-box-style-2 align-items-center">
+                                                <div class="feature-box-icon">
+                                                    <img width="30" height="30" src="img/demos/law-firm/icons/phone.svg" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-stroke-color-primary p-relative left-3'}" style="width: 30px; height: 30px;" />
+                                                </div>
+                                                <div class="feature-box-info ps-1">
+                                                    <label>CALL US NOW</label>
+                                                    <strong><a href="tel:+919048550123">+91 9048550123</a></strong>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="align-items-center d-none d-sm-block h-100 py-4">
+                                        <div class="header-extra-info-text h-100 py-2">
+                                            <div class="feature-box feature-box-style-2 align-items-center">
+                                                <div class="feature-box-icon">
+                                                    <img width="33" height="31" src="img/demos/law-firm/icons/comment.svg" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-primary'}" style="width: 33px; height: 31px;" />
+                                                </div>
+                                                <div class="feature-box-info ps-1">
+                                                    <label class="p-relative top-4">GET STARTED</label>
+                                                    <strong class="text-uppercase"><a href="#">Request Consultation <img width="27" height="27" src="img/demos/law-firm/icons/arrow-right.svg" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-primary ms-2 d-inline'}" /></a></strong>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-nav-bar">
+                    <div class="container">
+                        <div class="header-row">
+                            <div class="header-column">
+                                <div class="header-row">
+                                    <div class="header-column">
 
-        <!-- Theme Base, Components and Settings -->
-        <script src="{{ public_path().'/js/theme.js' }}"></script>
+                                        <div class="header-nav justify-content-start header-nav-line header-nav-bottom-line header-nav-bottom-line-effect-1">
+                                            <div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
+                                                <nav class="collapse">
+                                                    <ul class="nav nav-pills" id="mainNav">
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item active" href="demo-law-firm.html">
+                                                                Home
+                                                            </a>
+                                                        </li>
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item" href="demo-law-firm-about-us.html">
+                                                                About Us
+                                                            </a>
+                                                        </li>
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item" href="demo-law-firm-attorneys.html">
+                                                                Attorneys
+                                                            </a>
+                                                        </li>
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item" href="demo-law-firm-practice-areas.html">
+                                                                Practice Areas
+                                                            </a>
+                                                        </li>										
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item" href="demo-law-firm-news.html">
+                                                                News
+                                                            </a>
+                                                        </li>
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item" href="demo-law-firm-contact-us.html">
+                                                                Contact Us
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </nav>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="header-column">
+                                        <div class="header-row justify-content-end py-2">
+                                            <div class="header-nav-features header-nav-features-no-border ms-0 ps-0 w-100">
+                                                <form role="search" class="d-flex w-100">
+                                                    <div class="simple-search input-group w-100">
+                                                        <input class="form-control bg-transparent border-0 border-radius-0 text-2" id="headerSearch" name="q" type="search" value="" placeholder="Search...">
+                                                        <button class="btn border-0" type="submit">
+                                                            <i class="fa fa-search text-color-primary header-nav-top-icon p-relative top-1"></i>
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
+                                        <i class="fas fa-bars"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </header>
+            @yield("content")
+            <!-- Vendor -->
+            <script src="{{ public_path().'/vendor/plugins/js/plugins.min.js' }}"></script>
 
-        <!-- Revolution Slider Scripts -->
-        <script src="{{ public_path().'/vendor/rs-plugin/js/jquery.themepunch.tools.min.js' }}"></script>
-        <script src="{{ public_path().'/vendor/rs-plugin/js/jquery.themepunch.revolution.min.js' }}"></script>
+            <!-- Theme Base, Components and Settings -->
+            <script src="{{ public_path().'/js/theme.js' }}"></script>
 
-        <!-- Current Page Vendor and Views -->
-        <script src="{{ public_path().'/js/views/view.contact.js' }}"></script>
+            <!-- Revolution Slider Scripts -->
+            <script src="{{ public_path().'/vendor/rs-plugin/js/jquery.themepunch.tools.min.js' }}"></script>
+            <script src="{{ public_path().'/vendor/rs-plugin/js/jquery.themepunch.revolution.min.js' }}"></script>
 
-        <!-- Demo -->
-        <script src="{{ public_path().'/js/demos/demo-law-firm.js' }}"></script>
+            <!-- Current Page Vendor and Views -->
+            <script src="{{ public_path().'/js/views/view.contact.js' }}"></script>
 
-        <!-- Theme Custom -->
-        <script src="{{ public_path().'/js/custom.js' }}"></script>
+            <!-- Demo -->
+            <script src="{{ public_path().'/js/demos/demo-law-firm.js' }}"></script>
 
-        <!-- Theme Initialization Files -->
-        <script src="{{ public_path().'/js/theme.init.js' }}"></script>
+            <!-- Theme Custom -->
+            <script src="{{ public_path().'/js/custom.js' }}"></script>
+
+            <!-- Theme Initialization Files -->
+            <script src="{{ public_path().'/js/theme.init.js' }}"></script>
+        </div>
     </body>
 </html>
